4e602c90801418804533b305e7d1ecc490f0c63f
FranckReynaud <franck_r971@hotmail.com>
2014-03-02 23:30:35 -0400

    implementation_classe_joueur
    légère implementation des méthode deplacer et gestionCollision

--- Main.java
+++ Main.java
@@ -4,15 +4,17 @@ package com.supinfo.sokoban;
 public class Main {
 
 	public static void main(String[] args) {
-		
-		Case [][] map = new Case[8][8];
-	
+
+		int largeurMap = 8;
+		int hauteurMap = 8;
+		Case [][] map = new Case[largeurMap][hauteurMap];
+
 			for(int i = 0; i < 8 ; i++){
 				for(int j =0; j < 8; j++){
 					map[i][j] = new Case();
 				}
 			}
-			
+
 			for(int i = 0; i < 8; i++){
 				for(int j =0; j < 8; j++){
 					map[i][j].afficherContenu();
