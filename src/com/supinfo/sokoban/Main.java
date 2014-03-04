97bd18887eac89e6c39fbc402d931be614049440
Elepole <blackhole.vs.darkhole@gmail.com>
2014-03-04 15:48:30 -0400

    Parser d'argument
    Comme indiquer dans le sumary

--- src/com/supinfo/sokoban/Main.java
+++ src/com/supinfo/sokoban/Main.java
@@ -1,56 +1,45 @@
 package com.supinfo.sokoban;
 
-
 public class Main {
 
 	public static void main(String[] args) {
 
-		int largeurMap = 8;
-		int hauteurMap = 8;
-		Case [][] map = new Case[largeurMap][hauteurMap];
+		String level = null;
 
-			for(int i = 0; i < 8 ; i++){
-				for(int j =0; j < 8; j++){
-					map[i][j] = new Case();
+		if (args.length >= 1) {
+		
+			if (args[0].equals("--create")) {
+				
+				Editor sokobanedit = new Editor();
+				
+			} else if (args[0].equals("--score")) {
+				
+				Score sokobanscore = new Score();
+				
+			} else if (args[0].equals("--level")) {
+				
+				if (args.length >= 2) {
+					
+					level = args[1];
+					Play sokoban = new Play(level);
+
+					
+				} else {
+					
+
+					Play sokoban = new Play();
+					
 				}
 			}
-
-			for(int i = 0; i < 8; i++){
-				for(int j =0; j < 8; j++){
-					map[i][j].afficherContenu();
-					if(j == 7)
-						System.out.println();
-				}
-			}
-			//on libère la mémoire allouée à la map ainsi on peut choisir de nouvelles dimensions au besoin
-			//Pour charger le niveau suivant par exemple :
 			
-			for(int i =0; i < largeurMap ; i++){
-				for(int j = 0; j < hauteurMap; j++){
-					map[i][j] = null;
-				}
-			}
-				map =null;
-				
-				
-				//Preuve à l'appui :
-				
-				System.out.println("level 2");
-				
-				map = new Case[10][20];
-				
-				for(int i = 0; i < 10 ; i++){
-					for(int j =0; j < 20; j++){
-						map[i][j] = new Case();
-					}
-				}
-				
-				for(int i = 0; i < 10; i++){
-					for(int j =0; j < 20; j++){
-						map[i][j].afficherContenu();
-						if(j == 19)
-							System.out.println();
-					}
-				}
+
+		} else {
+
+			Play sokoban = new Play();
 		}
+		
+		
+	}
+	
+	
 }
--- src/com/supinfo/sokoban/Score.java
+++ src/com/supinfo/sokoban/Score.java
@@ -1,10 +1,5 @@
 package com.supinfo.sokoban;
 
 public class Score {
-	
-	private int scrore;
-	private String fileName;
-	
-	public FileWriter(String fileName);
 
 }
