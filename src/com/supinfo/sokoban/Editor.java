f074bc08b068134fe908d0eb86e760c9578a727a
r3fresh <solcraft@hotmail.fr>
2014-03-06 19:06:43 -0400

    maj editor

--- src/com/supinfo/sokoban/Editor.java
+++ src/com/supinfo/sokoban/Editor.java
@@ -4,11 +4,14 @@ import java.util.Scanner;
 
 public class Editor {
 
-	// lignes que l'utilisateur va entrer
+	// On créé un string dans lequel le joueur entrera le 8 lignes de la map
 	String ligne;
+	// Cible passera a 1 si le caractère "O" est repéré lors de la vérification
+	// d'un caractere d'une ligne
 	int cible = 0;
 	private Plateau plateau;
 	Character t;
+	Character j;
 	int nbJoueur;
 	int nbCibles;
 
@@ -16,23 +19,24 @@ public class Editor {
 
 		ligne = new String();
 		plateau = new Plateau();
-		nbCibles=0;
+		nbCibles = 0;
 
-		// Sera egale a 1 une fois la ligne bien valider
+		// l est un entier qui sera égale a 1 une fois les lignes bien validées
 		int l = 0;
 
 		System.out.println("Bienvenu dans l'editeur");
 		System.out.println("Creation du niveau");
 		Scanner sc = new Scanner(System.in);
 
-		// Saisi de la Première ligne
+		// Saisi des lignes
 		for (int x = 0; x < 8; x++) {
 			do {
 				System.out.print("Saisir  ligne:");
 				ligne = sc.nextLine();
 
 				if (ligne.length() == 8) {
-
+					
+                 
 					l = 1;
 
 				}
@@ -49,29 +53,24 @@ public class Editor {
 
 			for (int y = 0; y < 8; y++) {
 				t = Character.valueOf(ligne.charAt(y));
-				
 
-				if (t.toString().equals("O")){
+				if (t.toString().equals("O")) {
 					cible = 1;
 					nbCibles++;
 				}
 				plateau.setPlateauCase(x, y, t.toString(), cible);
 				cible = 0;
 			}
-			
-			 
 
 		}
-		
-		 System.out.println("Donner un nom a votre map");
-		 ligne = sc.nextLine();
-		 plateau.setNomPlateau(ligne);
-		 plateau.setNb_cibles(nbCibles);
-		 
-		 SaveMap saveMap = new SaveMap(plateau);
+
+		System.out.println("Donner un nom a votre map");
+		ligne = sc.nextLine();
+		plateau.setNomPlateau(ligne);
+		plateau.setNb_cibles(nbCibles);
+
+		SaveMap saveMap = new SaveMap(plateau);
 		sc.close();
-		
-		
 
 		plateau.afficherPlateau();
 
